c79b720f070dc3f7b9ec15bd80bd1cb9cb89fa4a
aozorarb <melon0802yota800@icloud.com>
2024-04-21 16:44:01 +0900

    add symbol key

---
+++ not_vim/scripts/symborak-us-create.rb
@@ -0,0 +1,70 @@
+# ruby ver.3.3.0
+# affected by https://github.com/mizunowanko/Symvorak/tree/SymbolOnly
+# karabiner-elements ver.14.13.0
+require 'json'
+require 'yaml'
+
+Alpha2Symbol = YAML.load_file('alpha2symbol-us.yml')
+
+Json = {
+  description: Alpha2Symbol['description'],
+  manipulators: [
+  ]
+}
+
+
+# set symbol key
+Json[:manipulators] << {
+  type: 'basic',
+  from: {
+    key_code: Alpha2Symbol['symbol'],
+    modifiers: {
+      mandatory: ['any']
+    },
+  },
+  to: [{
+    set_variable: {
+      name: 'symbol',
+      value: true
+    }
+  }],
+  to_after_key_up: [{
+    set_variable: {
+      name: 'symbol',
+      value: false
+    }
+  }]
+}
+
+Alpha2Symbol['swap_keys'].each do |k, v|
+  from_keycode = k
+  partitioned = v.partition('-')
+  is_with_shift = (partitioned[0] == 'S')
+  to_keycode = (is_with_shift ? partitioned[2] : partitioned[0])
+
+  Json[:manipulators] << {
+    type: 'basic',
+    conditions: [{
+      type: 'variable_if',
+      name: 'symbol',
+      value: true
+    }],
+    from: {
+      key_code: from_keycode,
+      modifiers: {
+        mandatory: ['any']
+      },
+    },
+    to: [{
+      key_code: to_keycode
+    }]
+  }
+  if is_with_shift
+    Json[:manipulators].last[:to][0][:modifiers] = ['shift']
+  end
+end
+
+
+File.open('symborak-us.json', 'w') do |file|
+  file.puts JSON.pretty_generate(Json)
+end
